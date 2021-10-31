5b99a9538e80b6c9420e7170c27c5a6c983e778b
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-10-31 17:24:38 -0400

    add more prints for different operations

--- ruby_basics/calc.rb
+++ ruby_basics/calc.rb
@@ -6,3 +6,7 @@ num_1 = gets.chomp
 puts "Eneter the second number"
 num_2 = gets.chomp
 puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
+puts "The first number plus the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number divided by the second number has a remainder of #{num_1.to_i % num_2.to_i}"
