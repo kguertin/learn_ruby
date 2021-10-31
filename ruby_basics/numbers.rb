58a3ed9d8c3d1ebda0fffb51ac51125001c05bef
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-10-31 17:15:46 -0400

    ruby number basics

---
+++ ruby_basics/numbers.rb
@@ -0,0 +1,25 @@
+puts 1 + 2
+
+puts 10 / 4
+puts 10.0 / 4
+puts 10 / 4.to_f
+
+x = 5
+y = 10
+puts x + y
+puts x * y
+puts y / x.to_f
+
+puts "5" * 2
+puts "-" * 20
+
+20.times {print "-"}
+puts "\n"
+puts rand(10)
+puts rand(100)
+
+a = "5".to_i
+b = "10".to_f
+
+puts b / a
+
