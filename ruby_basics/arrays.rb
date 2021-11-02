0bfe3dabff138b12378ea1d8f8d76360ccc4b80d
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-11-02 18:54:41 -0400

    iterators

--- ruby_basics/arrays.rb
+++ ruby_basics/arrays.rb
@@ -17,3 +17,35 @@ p y.shuffle
 p y 
 p y.reverse!
 p y
+
+abc = a << 10
+p abc.first
+p abc.last
+p abc.unshift("first")
+p abc.append("lasr")
+p abc.pop
+p abc.shift
+p abc
+
+p abc.include?(1)
+
+b = abc.join("-")
+p b
+p b.split("-")
+
+p %w("This is a test")
+
+for i in abc 
+    p i
+end
+
+abc.each do |i|
+    print "#{i} "
+end
+
+puts
+
+abc.each {|i| print "test "}
+
+item = (1..100).to_a.shuffle
+p z.select {|number| number.odd?}
