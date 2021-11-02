a7eb11567099f9d03f0239cbe2c1524b4fdd105e
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-11-02 18:22:00 -0400

    functions in ruby

---
+++ ruby_basics/methods.rb
@@ -0,0 +1,22 @@
+def multiply(first_num, second_num)
+    first_num.to_f * second_num.to_f
+end
+
+def add(first_num, second_num)
+    first_num.to_f + second_num.to_f
+end
+
+def subtract(first_num, second_num)
+    first_num.to_f - second_num.to_f
+end
+
+def divide(first_num, second_num)
+    first_num / second_num
+end 
+
+
+puts multiply(5, 8)
+puts add(3, 5)
+puts(5 - 3)
+puts(20 / 2)
+
