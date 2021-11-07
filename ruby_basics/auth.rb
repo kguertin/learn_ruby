8dd149047faff52502f61d27d984910dcdcbadc3
Kevin Guertin <kevin.m.guertin@gmail.com>
2021-11-07 17:27:32 -0500

    start authenticator

---
+++ ruby_basics/auth.rb
@@ -0,0 +1,9 @@
+users = [
+{username: "kevin" password: "password"}
+]
+
+puts "Welcome to the authenticator"
+25.times { print("-") }
+puts
+puts "This Program will take input from the user and compare password"
+puts "If password is correct you will get back the user object"
